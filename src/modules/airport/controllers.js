1c27d239d598cc38bb014d01314f0cf79c60557f
fajrCode <fajri42vandi@gmail.com>
2024-12-16 13:35:50 +0700

    feat: airport | crud complete

--- src/modules/airport/controllers.js
+++ src/modules/airport/controllers.js
@@ -1,46 +1,20 @@
 import * as AirportService from './services.js';
+import { getAllCity } from '../city/services.js';
 
 export const index = async (req, res, next) => {
     try {
         const airports = await AirportService.getAllAirport();
-
+        const cities = await getAllCity();
+        const api = process.env.API_URL;
         const data = {
             title: 'Airports',
-            airports
-        }
+            airports,
+            cities,
+            api
+        };
 
         res.edge('pages/airport/index', data);
     } catch (error) {
         next(error)
-    }
-};
-
-export const createAirport = async (req, res, next) => {
-    try {
-        const api = process.env.API_URL;
-        const data = {
-            title: 'Airport',
-            sub: 'Create',
-            api
-        }
-
-        res.edge('pages/airport/create', data);
-    } catch (error) {
-        next(error)
-    }
-};
-
-export const editAirport = async (req, res, next) => {
-    try {
-        const airport = await AirportService.getAirportById(req.params.id);
-        const data = {
-            title: 'Airport',
-            sub: 'Edit',	
-            airport
-        }
-
-        res.edge('pages/airport/edit', data);
-    } catch (error) {
-        next(error)
-    }
+    };
 };
