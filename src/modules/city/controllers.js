483da81d7d6343571502f65e93e5ff9eedf60ce3
fajrCode <fajri42vandi@gmail.com>
2024-12-17 11:04:02 +0700

    feat: city and country | complete crud

--- src/modules/city/controllers.js
+++ src/modules/city/controllers.js
@@ -17,3 +17,39 @@ export const index = async (req, res, next) => {
         next(error)
     };
 };
+
+export const edit = async (req, res, next) => {
+    try {
+        const city = await CityService.getCityById(req.params.id);
+        const countries = await CityService.getAllCountry();
+        const api = process.env.API_URL;
+        const data = {
+            title: 'City & Country',
+            city,
+            countries,
+            api
+        };
+
+        console.log(city.code)
+
+        res.edge('pages/city/edit', data);
+    } catch (error) {
+        next(error)
+    };
+};
+
+export const editCountry = async (req, res, next) => {
+    try {
+        const country = await CityService.getCountryById(req.params.id);
+        const api = process.env.API_URL;
+        const data = {
+            title: 'City & Country',
+            country,
+            api
+        };
+
+        res.edge('pages/city/edit-country', data);
+    } catch (error) {
+        next(error)
+    };
+};
--- src/modules/city/services.js
+++ src/modules/city/services.js
@@ -11,8 +11,31 @@ export const getAllCity = async () => {
     return result.data;
 };
 
+export const getCityById = async (id) => {
+    const response = await fetch(process.env.API_URL + '/api/v1/cities/' + id);
+    const result = await response.json();
+
+    if (response.status !== 200) {
+        throw new ErrorCustom(result.message, response.code);
+    }
+
+    return result.data;
+
+};
+
 export const getAllCountry = async () => {
     const response = await fetch(process.env.API_URL + '/api/v1/countries');
+    const result = await response.json();
+
+    if (response.status !== 200) {
+        throw new ErrorCustom(result.message, response.code);
+    }
+
+    return result.data;
+};
+
+export const getCountryById = async (id) => {
+    const response = await fetch(process.env.API_URL + '/api/v1/countries/' + id);
     const result = await response.json();
 
     if (response.status !== 200) {
