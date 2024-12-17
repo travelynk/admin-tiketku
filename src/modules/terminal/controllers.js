c2b68bc3cdb96d0c27ed7b54671a1abb3b3334db
fajrCode <fajri42vandi@gmail.com>
2024-12-17 11:47:48 +0700

    feat: terminal | update, make edit pages

--- src/modules/terminal/controllers.js
+++ src/modules/terminal/controllers.js
@@ -20,3 +20,24 @@ export const index = async (req, res, next) => {
         next(error)
     }
 };
+
+export const edit = async (req, res, next) => {
+    try {
+        const token = req.session.token;
+        const id = req.params.id;
+        const terminal = await TerminalService.getTerminalById(id, token);
+        const airports = await getAllAirport(token);
+        const api = process.env.API_URL;
+        console.log(terminal)
+        const data = {
+            title: 'Terminal',
+            api,
+            terminal,
+            airports
+        }
+
+        res.edge('pages/terminal/edit', data);
+    } catch (error) {
+        next(error)
+    }
+};
