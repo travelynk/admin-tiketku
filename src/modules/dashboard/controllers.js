991f991a1a3946ec55178b37aa15ae2ec870e00a
fajrCode <fajri42vandi@gmail.com>
2024-12-17 12:14:51 +0700

    feat: dashboard complete get data

--- src/modules/dashboard/controllers.js
+++ src/modules/dashboard/controllers.js
@@ -1,10 +1,25 @@
-import * as DashboardService from './services.js';
+import { getAllCity, getAllCountry } from '../city/services.js';
+import { getAirlines } from '../airline/services.js';
+import { getFlights } from '../flight/services.js';
 
 export const index = async (req, res, next) => {
     try {
-        const data = await DashboardService.index();
+        const token = req.session.token;
+        const cities = await getAllCity();
+        const countries = await getAllCountry();
+        const airlines = await getAirlines(token);
+        const flights = await getFlights(token);
 
-        res.edge('pages/dashboard/index', { login: data.result, foods: data.foods });
+        const data = {
+            title: 'Dashboard',
+            citiesQty: cities.length,
+            countriesQty: countries.length,
+            airlinesQty: airlines.length,
+            flightsQty: flights.length,
+            user: req.session.user
+        }
+
+        res.edge('pages/dashboard/index', data);
     } catch (error) {
         next(error)
     }
